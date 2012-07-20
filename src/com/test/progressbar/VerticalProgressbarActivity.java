273132ecbb31e16be7b1f62333dbba06b08fa59d
carlos <huaixian.huang@gmail.com>
2012-07-20 16:32:17 +0800

    some note about progress_color_vertical.xml

--- src/com/test/progressbar/VerticalProgressbarActivity.java
+++ src/com/test/progressbar/VerticalProgressbarActivity.java
@@ -10,23 +10,26 @@ import android.view.View.OnClickListener;
 public class VerticalProgressbarActivity extends Activity {
 	ProgressBar progress;
 	static int i = 0;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        progress = (ProgressBar) findViewById(R.id.t320100_mypb);
-        Button setBtn = (Button)findViewById(R.id.button1);
-        setBtn.setOnClickListener(new OnClickListener() {
-			
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		progress = (ProgressBar) findViewById(R.id.t320100_mypb);
+		Button setBtn = (Button) findViewById(R.id.button1);
+		progress.setProgress(100);
+
+		setBtn.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				i = i + 10;
-		        progress.setProgress(i);
+				progress.setProgress(i);
 			}
 		});
-//        progress.setProgress(50);
-        
-    }
+		// progress.setProgress(50);
+
+	}
 }
